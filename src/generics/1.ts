6189483130a274613ec219f5a542e29a2fee6cc4
Nelli Diachkina <nelli.diachkina@gmail.com>
2024-07-03 10:28:04 +0300

    init

---
+++ src/generics/1.ts
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+// async function fetchData(url) {
+//   try {
+//     const response = await axios.get(url);
+//     return response.data;
+//   } catch (error) {
+//     throw new Error(`Error fetching from ${url}: ${error}`);
+//   }
+// }
